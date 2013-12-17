c24ce246bf4a5977c16fcc6b2e9579710e4f97cf
stefangs <stefangs@nethome.nu>
2013-12-17 21:42:18 +0100

    Added possibility to configure log file location via -l parameter

--- server/src/main/java/nu/nethome/home/impl/HomeManagerStarter.java
+++ server/src/main/java/nu/nethome/home/impl/HomeManagerStarter.java
@@ -62,17 +62,7 @@ public class HomeManagerStarter {
         HomeServer server = new HomeServer();
         server.setName("Home Server");
 
-        try {
-            // Initialize logging ( http://java.sun.com/j2se/1.4.2/docs/api/java/util/logging/package-summary.html )
-            String logFileName = System.getProperty("user.home") + "/HomeManager%g.log";
-            Handler fh = new FileHandler(logFileName, LOG_FILE_SIZE, NUMBER_OF_LOGFILES, true);
-            fh.setFormatter(new LogFormatter());
-            Logger.getLogger("").addHandler(fh);
-            logger.info("**Starting HomeManager " + HomeManagerStarter.class.getPackage().getImplementationVersion() + "**");
-            logger.info("Logging to: " + logFileName);
-        } catch (IOException e) {
-            e.printStackTrace(System.err);
-        }
+        String logFileName = System.getProperty("user.home");
 
         // Check arguments - if no arguments, load the demo configuration
         int i = 0;
@@ -82,7 +72,22 @@ public class HomeManagerStarter {
             if (args[i].startsWith("-p")) {
                 pluginDirectories.add(args[i].substring(2));
             }
+            if (args[i].startsWith("-l")) {
+                logFileName = args[i].substring(2);
+            }
             i++;
+        }
+
+        try {
+            // Initialize logging ( http://java.sun.com/j2se/1.4.2/docs/api/java/util/logging/package-summary.html )
+            logFileName += "/HomeManager%g.log";
+            Handler fh = new FileHandler(logFileName, LOG_FILE_SIZE, NUMBER_OF_LOGFILES, true);
+            fh.setFormatter(new LogFormatter());
+            Logger.getLogger("").addHandler(fh);
+            logger.info("**Starting HomeManager " + HomeManagerStarter.class.getPackage().getImplementationVersion() + "**");
+            logger.info("Logging to: " + logFileName);
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
         }
 
         // Create Plugin scanner and scan for plugins
